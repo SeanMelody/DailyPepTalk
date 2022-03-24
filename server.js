8a96904e86b0427861dd4e18493462ba57633837
Sean Melody <seandmelody@gmail.com>
2022-03-23 17:03:50 -0700

    More NPM packages added

--- server.js
+++ server.js
@@ -4,23 +4,22 @@ const app = express();
 require("dotenv").config();
 
 
-//Set up the port for Heroku depoloyment and development to 5050 cause I'm crazy!
+//Set up the port for Heroku deployment and development to 5050 cause I'm crazy!
 const PORT = process.env.PORT || 5050;
 
 
 // Middleware
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Herku Optimization
+// Heroku Optimization
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
 }
 
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
