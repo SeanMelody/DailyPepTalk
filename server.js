f4987d02e976973aeeb4be3c496a50c285c891c8
Sean Melody <seandmelody@gmail.com>
2022-03-24 12:20:24 -0700

    routes file name changed to api

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"))
 }
 
-app.use("/", require("./routes/quotesRoutes"))
+app.use("/api", require("./routes/apiRoutes"))
 
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
