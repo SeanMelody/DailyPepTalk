622039d7691aaf48e8614e87b3fdb44609a81e51
Sean Melody <seandmelody@gmail.com>
2022-03-29 12:02:52 -0700

    email sending as HTML now instead of plain text

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -27,7 +27,7 @@ router.post("/submittedQuote", (req, res, next) => {
         Author: ${req.body.author}
         Submitted By: ${req.body.submittedBy}
         Email: Need to set up email
-        Tags: ${req.body.Tags}`
+        Tags: ${req.body.tags}`
     console.log(newQuote)
 
     const subject = `New Submitted Quote from ${req.body.submittedBy}`
@@ -36,7 +36,14 @@ router.post("/submittedQuote", (req, res, next) => {
         from: process.env.EMAIL,
         to: process.env.EMAIL,
         subject: subject,
-        text: newQuote
+        html: `<stlye>
+        <h3>Author: ${newQuote.author}</h3>
+        <p>Quote: ${newQuote.quote}</p>
+        <h4>Submitted by: ${newQuote.submittedBy}</h4>
+        <h4><a href="google.com">Email: Need to set this up</a></h4>
+        <p>Tags: </p>
+        <img src="/client/public/Favicon.png">
+        `
     }
 
     // Send the mail and check for errors with transporter and nodemailer
