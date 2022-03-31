eb52bf41006455b79824c36f2e7de9df2a34ec0e
Sean Melody <seandmelody@gmail.com>
2022-03-31 10:56:46 -0700

    Quotes from external endpoint set to state and displaying

--- client/src/Pages/ExternalQuote.js
+++ client/src/Pages/ExternalQuote.js
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const ExternalQuote = () => {
 
+    const [extQuote, setExtQuote] = useState({
+        quote: "",
+        author: ""
+    })
 
     const quoteGen = async () => {
-
-        const getQuote = await axios.get("/api/externalquote")
-        console.log(getQuote.data)
+        try {
+            const getQuote = await axios.get("/api/externalquote")
+            console.log(getQuote.data)
+            setExtQuote(getQuote.data)
+        } catch (error) {
+            console.log(error)
+        }
 
     }
+
+    // useEffect(() => {
+    //     quoteGen()
+
+    // }, [])
+
 
     return (
         <div>ExternalQuote
 
             <h1>Press Me for a quote!</h1>
             <button onClick={quoteGen}>Quote</button>
+
+
+            <div>
+                <p>{extQuote.quote}</p>
+                <h3>{extQuote.author}</h3>
+
+            </div>
+
+
+
         </div>
     )
 }
