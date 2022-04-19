1cb1d8588451c8a1d561e36f5ef691599f86c2dd
Sean Melody <seandmelody@gmail.com>
2022-04-19 16:52:29 -0700

    Not found page styled

--- client/src/Pages/NotFound.js
+++ client/src/Pages/NotFound.js
@@ -4,11 +4,19 @@ import { Link } from "react-router-dom"
 const NotFound = () => {
     return (
         <div>
-            <h3>Oops</h3>
-            <img src="./obi-wan.jpg"></img>
-            <h5>You want to go back <Link to="/">
-                <h5>Home</h5>
-            </Link></h5>
+            <h1 className="space-large">404 ERROR</h1>
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <img src="./obi-wan.jpg" alt="Obi-Wan wrong page"></img>
+                    <div className="row justify-content-center">
+                        <div className="col-md-12 space-large">
+                            <h5>You want to go back <Link to="/">
+                                <h5 className="link-design">Home</h5>
+                            </Link></h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
