643d1a1cd5b2cc88b4072f6b07348f9e167493e0
Sean Melody <seandmelody@gmail.com>
2022-06-30 10:57:02 -0700

    Loading text added after extrnal quote button pressed

--- client/src/Pages/ExternalQuote.js
+++ client/src/Pages/ExternalQuote.js
@@ -8,15 +8,24 @@ const ExternalQuote = () => {
         author: ""
     })
 
+    const [loading, setLoading] = useState({
+        loading: false
+    })
+
     const quoteGen = async () => {
+
+        setLoading(true)
         try {
             const getQuote = await axios.get("/api/externalquote")
+
             console.log(getQuote.data)
             setExtQuote(getQuote.data)
+            setLoading(false)
         } catch (error) {
             console.log(error)
         }
-
+        console.log("loading done")
+        // setLoading(false)
     }
 
     return (
@@ -24,8 +33,8 @@ const ExternalQuote = () => {
             <hr></hr>
             <h3>You reached the end of the Quotes</h3>
             <h5>But if you would like a randomly generated quote, hit this button!</h5>
-            <button className="btn btn-outline-dark" onClick={quoteGen}>Quote</button>
-
+            <button className="btn btn-outline-dark margin-small" onClick={quoteGen}>Random Quote</button>
+            {loading.true ? <h5 className="margin-small">...Loading</h5> : null}
 
             <div>
                 <br></br>
